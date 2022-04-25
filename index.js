d10d74dcb6a8bdd161185074a07bb498bd13f2d9
MaheepK9 <maheepkumar972000@gmail.com>
2022-04-26 00:01:51 +0530

    commit

---
+++ index.js
@@ -0,0 +1,62 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var phone_no = req.body.phone_no;
+    var student_no = req.body.student_no;
+    var section = req.body.section;
+    var branch = req.body.branch;
+    
+
+
+    var data = {
+        "name": name,
+        "email" : email,
+        "phone_no": phone_no,
+        "student_no" : student_no,
+        "section" : section,
+        "branch" : branch
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    
+
+})
+
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(3000);
+
+
+console.log("Listening on PORT 3000");
